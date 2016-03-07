f4f436ac1c9b45050e35807bca938b06df7682c8
Beverly Lau <beverly.a.lau@gmail.com>
2016-03-07 15:55:32 -0800

    Test concatenating string arrays

--- linebreak_test.rb
+++ linebreak_test.rb
@@ -2,9 +2,17 @@ class LinebreakTest
   def linebreaks
     a = %w(cat dog bird elephant llama giraffe turtle panda bear
       snake)
-    puts a
+    print a
+  end
+
+  def concat
+    a = %w(cat dog bird)
+    b = %w(elephant llama giraffe)
+    c = a + b
+    print c
   end
 end
 
 l = LinebreakTest.new
 l.linebreaks
+l.concat
